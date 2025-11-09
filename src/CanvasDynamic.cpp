6dc60b212b87b2e59b8ad8d8fb805d381c0f9df4
RugiSerl <cooletouf2@gmail.com>
2025-11-09 17:30:58 +0100

    fixed CanvasDynamic to have no artefacts during resizing

--- src/CanvasDynamic.cpp
+++ src/CanvasDynamic.cpp
@@ -14,8 +14,18 @@ namespace UIComponent {
         Canvas::update();
         Vector2 screenSpaceSize = GetScreenSpaceRectangle().GetSize();
         if (screenSpaceSize.x != texture.width || screenSpaceSize.y != texture.height) {
+            // Making a temporary copy of our content.
             raylib::RenderTexture tmp = std::move(renderTexture);
             renderTexture = raylib::RenderTexture(static_cast<int>(screenSpaceSize.x), static_cast<int>(screenSpaceSize.y));
+
+            // Drawing the old content onto the new one to avoid artifacts from random memory in the GPU (since
+            // the renderTexture was initialized without any content).
+            BeginTextureMode(renderTexture);
+                ClearBackground(BLACK);
+                DrawTextureRec(tmp.texture, raylib::Rectangle(0, texture.height, texture.width, -texture.height), raylib::Vector2(0, 0), WHITE);
+            EndTextureMode();
+
+            // Finally unload that copy to free space.
             tmp.Unload();
             texture = renderTexture.GetTexture();
         }
